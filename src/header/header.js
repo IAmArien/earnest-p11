a5e47e2a161232ecac5515d816fbfb81244d76ff
Norman Palisoc <norman.palisoc@novare.com.hk>
2023-12-10 21:36:20 +0800

    feat: WIP in header UI navigation items

--- src/header/header.js
+++ src/header/header.js
@@ -1,33 +1,44 @@
 import React from 'react'
-import { Container, Image, Navbar, Nav, NavDropdown } from 'react-bootstrap'
+import { Container, Image, Navbar, Nav, NavDropdown, Button } from 'react-bootstrap'
 import './css/header.css'
 
 export default function Header() {
     return (
         <React.Fragment>
-            <Navbar expand='lg' className='justify-content-between bg-light'>
+            <Navbar expand='lg' className='justify-content-between bg-light nav-header-container'>
                 <Container>
                     <Navbar.Brand href='./'>
                         <Image className='img-nav-logo' src='https://earnest.ph/invest/data/photos/earnest-logo.svg'/>
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Link href="#home" active={true}>Home</Nav.Link>
-                            <Nav.Link href="#lessons">Lessons</Nav.Link>
-                            <NavDropdown title="Basics" id="basic-nav-dropdown">
+                        <Nav className='nav-menu-items'>
+                            <Nav.Item>
+                                <Nav.Link className='auto-padder' href="#home" active={true}>Home</Nav.Link>
+                                <div className='nav-hover.active'></div>
+                            </Nav.Item>
+                            <Nav.Link className='auto-padder' href="#lessons">Lessons</Nav.Link>
+                            <NavDropdown className='auto-padder' title="Basics" id="basic-nav-dropdown">
                                 <NavDropdown.Item href="#explainers">Explainers</NavDropdown.Item>
                                 <NavDropdown.Item href="#dictionary">Dictionary</NavDropdown.Item>
                                 <NavDropdown.Item href="#moneymyths">Money Myths</NavDropdown.Item>
                             </NavDropdown>
-                            <NavDropdown title="Life" id="basic-nav-dropdown">
+                            <NavDropdown className='auto-padder' title="Life" id="basic-nav-dropdown">
                                 <NavDropdown.Item href="#stories">Stories</NavDropdown.Item>
                                 <NavDropdown.Item href="#howtos">How To's</NavDropdown.Item>
                                 <NavDropdown.Item href="#digests">Digests</NavDropdown.Item>
                             </NavDropdown>
-                            <Nav.Link href="#tools">Tools</Nav.Link>
+                            <Nav.Link className='auto-padder' href="#tools">Tools</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
+                    <div style={{ height: '100%' }}>
+                        <Nav.Link className='pull-left' href="https://technet.azure-api.net/public/v1/authentication/login?client_id=5fcfb64b-a29d-4845-98f0-ed75200b9f2c&amp;redirect_uri=https://earnest.ph/invest/callback&amp;response_type=CODE&amp;scope=offline_access">
+                            <Button className='btn-login-signup' variant='primary' size='md'>Login/Sign up</Button>
+                        </Nav.Link>
+                        <Nav.Item className='pull-right nav-item-search-icon'>
+                            <Image className='img-nav-search' src='https://earnest.ph/invest/data/photos/search_dark.svg' />
+                        </Nav.Item>
+                    </div>
                 </Container>
             </Navbar>
         </React.Fragment>
