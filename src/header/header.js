7720b15f41c6911d39443c8cb99012b5441b0be3
Norman Palisoc <norman.palisoc@digitalspaceexplorer.com>
2023-12-16 13:58:54 +0800

    feat: add search bar in navigation header

--- src/header/header.js
+++ src/header/header.js
@@ -3,7 +3,57 @@ import { Container, Image, Navbar, Nav, Button } from 'react-bootstrap';
 
 import './css/header.css';
 
+import HEADER_SEARCH from '../sections/resources/HEADER_SEARCH.svg';
+import HEADER_CLOSE from '../sections/resources/HEADER_CLOSE.svg';
+
 export default function Header() {
+
+    const hideSearchIcon =() => {
+        const navSearchCloseIcon = document.getElementById('imgNavSearchClose');
+        const navSearchIcon = document.getElementById('imgNavSearch');
+        navSearchIcon.style.display = 'none';
+        navSearchCloseIcon.style.display = 'block';
+    }
+
+    const showSearchIcon =() => {
+        const navSearchCloseIcon = document.getElementById('imgNavSearchClose');
+        const navSearchIcon = document.getElementById('imgNavSearch');
+        navSearchIcon.style.display = 'block';
+        navSearchCloseIcon.style.display = 'none';
+    }
+
+    const onNavSearchClick =() => {
+
+        hideSearchIcon()
+
+        const navItemLoginSignup = document.getElementById('navItemLoginSignup');
+        navItemLoginSignup.classList.remove('navItemLoginSignup-visible')
+        navItemLoginSignup.classList.add('navItemLoginSignup-hidden')
+
+        const navItemSearchBar = document.getElementById('navItemSearchBar');
+        navItemSearchBar.classList.add('navItemSearchBar-default')
+
+        const navInputSearchBar = document.getElementById('navInputSearchBar');
+        navInputSearchBar.classList.remove('navInputSearchBar-no-width')
+        navInputSearchBar.classList.add('navInputSearchBar-width')
+    }
+
+    const onNavSearchCloseClick =() => {
+
+        showSearchIcon()
+
+        const navItemLoginSignup = document.getElementById('navItemLoginSignup');
+        navItemLoginSignup.classList.remove('navItemLoginSignup-hidden')
+        navItemLoginSignup.classList.add('navItemLoginSignup-visible')
+
+        const navItemSearchBar = document.getElementById('navItemSearchBar');
+        navItemSearchBar.classList.remove('navItemSearchBar-default')
+
+        const navInputSearchBar = document.getElementById('navInputSearchBar');
+        navInputSearchBar.classList.remove('navInputSearchBar-width')
+        navInputSearchBar.classList.add('navInputSearchBar-no-width')
+    }
+
     return (
         <React.Fragment>
             <Navbar expand='lg' fixed='top' className='justify-content-between nav-bg-light nav-header-container'>
@@ -55,11 +105,15 @@ export default function Header() {
                         </Nav>
                     </Navbar.Collapse>
                     <div style={{ height: '100%' }}>
-                        <Nav.Link className='pull-left' href="https://technet.azure-api.net/public/v1/authentication/login?client_id=5fcfb64b-a29d-4845-98f0-ed75200b9f2c&amp;redirect_uri=https://earnest.ph/invest/callback&amp;response_type=CODE&amp;scope=offline_access">
+                        <Nav.Link id='navItemLoginSignup' className='pull-left navItemLoginSignup-visible ' href="https://technet.azure-api.net/public/v1/authentication/login?client_id=5fcfb64b-a29d-4845-98f0-ed75200b9f2c&amp;redirect_uri=https://earnest.ph/invest/callback&amp;response_type=CODE&amp;scope=offline_access">
                             <Button className='btn-login-signup' variant='primary' size='md'>Login/Sign up</Button>
                         </Nav.Link>
+                        <Nav.Item id='navItemSearchBar' className='pull-left navInputSearchBar-default'>
+                            <input id='navInputSearchBar' className="input-header-search" type="email" placeholder="Search Earnest..." />
+                        </Nav.Item>
                         <Nav.Item className='pull-right nav-item-search-icon'>
-                            <Image className='img-nav-search' src='https://earnest.ph/invest/data/photos/search_dark.svg' />
+                            <Image id='imgNavSearch' onClick={() => onNavSearchClick()} className='img-nav-search' src={HEADER_SEARCH} />
+                            <Image id='imgNavSearchClose' onClick={() => onNavSearchCloseClick()} className='img-nav-search-close' src={HEADER_CLOSE} />
                         </Nav.Item>
                     </div>
                 </Container>
